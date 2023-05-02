f27fde26a010001023beea507471c7d2f25d7823
tintun123 <mr.tintunoo1997@gmail.com>
2023-05-02 10:17:35 +0630

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="">
+  <div class="relative">
+    <div class="absolute top-[3rem] right-[-3rem] w-[153px] h-[133px]">
+      <svg width="100%" height="100%" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="76.5" cy="66.5" rx="76.5" ry="66.5" fill="#1153BD" fill-opacity="0.4"/>
+      </svg>
+    </div>
     <h1 id="logo" class="mx-4 text-minor-xlg mt-12 mb-16 text-primary font-bold">
       Enid
     </h1>
 
     <div class="form mx-4">
       <form>
-        <!-- <div class="relative mb-3" data-te-input-wrapper-init>
-          <input
-            type="text"
-            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-            id="exampleFormControlInput1"
-            placeholder="Example label" />
-          <label
-            for="exampleFormControlInput1"
-            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >Example label
-          </label>
-        </div> -->
         <div class="username relative">
           <input type="text" name="username" id="username"
           class="
@@ -51,7 +44,6 @@
             left-3 
             top-0 
             mb-0 
-            max-w-[90%] 
             origin-[0_0] 
             truncate 
             pt-[0.37rem] 
@@ -60,9 +52,16 @@
             duration-200 
             ease-out 
             peer-focus:-translate-y-[1.5rem] 
-            peer-focus:scale-[0.8] 
+            peer-focus:scale-[0.9] 
             peer-focus:text-primary 
-            peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+            peer-focus:font-semibold
+            peer-focus:pt-0
+            peer-focus:left-0
+            peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
+            peer-data-[te-input-state-active]:scale-[0.8] 
+            motion-reduce:transition-none 
+            dark:text-school-name/50 
+            dark:peer-focus:text-primary"
             >Username
           </label>
         </div>
@@ -109,9 +108,16 @@
             duration-200 
             ease-out 
             peer-focus:-translate-y-[1.5rem] 
-            peer-focus:scale-[0.8] 
+            peer-focus:scale-[0.9] 
             peer-focus:text-primary 
-            peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+            peer-focus:font-semibold
+            peer-focus:pt-0
+            peer-focus:left-0
+            peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
+            peer-data-[te-input-state-active]:scale-[0.8] 
+            motion-reduce:transition-none 
+            dark:text-school-name/50
+            dark:peer-focus:text-primary"
             >
             Password
           </label>
@@ -132,6 +138,7 @@
           ease-linear 
           focus:placeholder:opacity-100 
           peer-focus:text-primary 
+          peer-focus:font-semibold
           data-[te-input-state-active]:placeholder:opacity-100 
           motion-reduce:transition-none
           rounded-[10px] 
@@ -159,10 +166,17 @@
             duration-200 
             ease-out 
             peer-focus:-translate-y-[1.5rem] 
-            peer-focus:scale-[0.8] 
+            peer-focus:scale-[0.9] 
             peer-focus:text-primary 
-            peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-            >Username
+            peer-focus:font-semibold
+            peer-focus:pt-0
+            peer-focus:left-0
+            peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
+            peer-data-[te-input-state-active]:scale-[0.8] 
+            motion-reduce:transition-none 
+            dark:text-school-name/50
+            dark:peer-focus:text-primary"
+            >Campus
           </label>
           <span class="absolute flex items-center inset-y-0 right-4">
             <svg fill="#5D5D72" opacity="0.6" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
@@ -171,188 +185,26 @@
       </form>
     </div>
 
-    <!-- <div class="grid grid-cols-4 mt-12 ml-4">
-      <div class="col-span-3 
-      min-h-[176px]
-      relative
-      rounded-[10px]
-
-      " id="card">
-      <div class="
-      content 
-      bg-white/60
-      h-full 
-      relative 
-      rounded-[10px] 
-      pt-4 
-      pl-2">
-        <div class="text-block">
-            <h1 class="text-minor-sm text-school-name font-semibold">
-            Cherry Language School
-          </h1>
-          <p class="
-          text-minor-xxsm 
-          line-clamp-6 
-          leading-[0.86875rem] 
-          text-school-name 
-          font-[0.6] 
-          mr-[0.5rem] 
-          mt-[0.51875rem]">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit iusto eligendi impedit voluptatibus harum atque quam nisi nihil ab voluptas enim nam necessitatibus praesentium minima distinctio, porro illum facilis reprehenderit.
-          </p>
-        </div>
-
-        <span class="absolute bottom-[0.625rem] flex items-center left-2 w-4 h-4">
-          <img src="facebook-cion.svg" class="block w-full h-full" alt="">
-        </span>
-
-        <span class="absolute bottom-[0.125rem] flex items-center right-[0.1975rem] w-4 h-4">
-          <img src="check-icon.svg" class="block w-full h-full " alt="">
-        </span>
-      </div>
-      </div>
-    </div> -->
-
     <div class="
-    mt-12 
+    mt-12
+    pr-4 
     pl-4
     flex
     flex-nowrap
+    gap-4
     overflow-x-scroll
     scrolling-touch
     pt-[1.25rem]
     "
     id="school-container">
-      <div class="
-      min-h-[176px]
-      relative
-      rounded-[10px]
-      ml-5
-      flex-none
-      w-3/4
-      " id="card">
-      <div class="
-      content 
-      bg-white/60
-      h-full 
-      relative 
-      rounded-[10px] 
-      pt-4 
-      pl-2">
-        <div class="text-block">
-            <h1 class="text-minor-sm text-school-name font-semibold">
-            Cherry Language School
-          </h1>
-          <p class="
-          text-minor-xxsm 
-          line-clamp-6 
-          leading-[0.86875rem] 
-          text-school-name 
-          font-[0.6] 
-          mr-[0.5rem] 
-          mt-[0.51875rem]">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit iusto eligendi impedit voluptatibus harum atque quam nisi nihil ab voluptas enim nam necessitatibus praesentium minima distinctio, porro illum facilis reprehenderit.
-          </p>
-        </div>
-
-        <span class="absolute bottom-[0.625rem] flex items-center left-2 w-4 h-4">
-          <img :src="facebookUrl" class="block w-full h-full" alt="">
-        </span>
-
-        <span class="absolute bottom-[0.125rem] flex items-center right-[0.1975rem] w-4 h-4">
-          <img :src="checkUrl" class="block w-full h-full " alt="">
-        </span>
-      </div>
-      </div>
-      <div class="col 
-      min-h-[176px]
-      relative
-      rounded-[10px]
-      ml-9
-      flex-none
-      w-3/4
-      " id="card">
-      <div class="
-      content 
-      bg-white/60
-      h-full 
-      relative 
-      rounded-[10px] 
-      pt-4 
-      pl-2
-      ">
-        <div class="text-block">
-            <h1 class="text-minor-sm text-school-name font-semibold">
-            Cherry Language School
-          </h1>
-          <p class="
-          text-minor-xxsm 
-          line-clamp-6 
-          leading-[0.86875rem] 
-          text-school-name 
-          font-[0.6] 
-          mr-[0.5rem] 
-          mt-[0.51875rem]">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit iusto eligendi impedit voluptatibus harum atque quam nisi nihil ab voluptas enim nam necessitatibus praesentium minima distinctio, porro illum facilis reprehenderit.
-          </p>
-        </div>
-
-        <span class="absolute bottom-[0.625rem] flex items-center left-2 w-4 h-4">
-          <img :src="facebookUrl" class="block w-full h-full" alt="">
-        </span>
-
-        <span class="absolute bottom-[0.125rem] flex items-center right-[0.1975rem] w-4 h-4">
-          <img :src="checkUrl" class="block w-full h-full " alt="">
-        </span>
-      </div>
-      </div>
-      <div class="col 
-      min-h-[176px]
-      relative
-      rounded-[10px]
-      ml-9
-      flex-none
-      w-3/4
-      " id="card">
-      <div class="
-      content 
-      bg-white/60
-      h-full 
-      relative 
-      rounded-[10px] 
-      pt-4 
-      pl-2">
-        <div class="text-block">
-            <h1 class="text-minor-sm text-school-name font-semibold">
-            Cherry Language School
-          </h1>
-          <p class="
-          text-minor-xxsm 
-          line-clamp-6 
-          leading-[0.86875rem] 
-          text-school-name 
-          font-[0.6] 
-          mr-[0.5rem] 
-          mt-[0.51875rem]">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit iusto eligendi impedit voluptatibus harum atque quam nisi nihil ab voluptas enim nam necessitatibus praesentium minima distinctio, porro illum facilis reprehenderit.
-          </p>
-        </div>
-
-        <span class="absolute bottom-[0.625rem] flex items-center left-2 w-4 h-4">
-          <img :src="facebookUrl" class="block w-full h-full" alt="">
-        </span>
-
-        <span class="absolute bottom-[0.125rem] flex items-center right-[0.1975rem] w-4 h-4">
-          <img :src="checkUrl" class="block w-full h-full " alt="">
-        </span>
-      </div>
-      </div>
+      <!-- <SchoolComponent v-for="school in model" :key="school.id" :school="school"></SchoolComponent> -->
+      <SchoolComponent v-for="school in model" :key="school.id" :school="school"></SchoolComponent>
     </div>
 
     <div class="
     login-btn
     mx-4
-    mt-[4.3125rem]">
+    mt-[4rem]">
     <button class="
     text-white 
     font-bold 
@@ -365,7 +217,7 @@
     w-full 
     py-1
     hover:bg-gradient-br
-    focus:ring-4
+    focus:ring-2
 
     focus:outline-none
     focus:ring-blue/50
@@ -378,14 +230,42 @@
 </template>
 
 <script setup>
-import facebookUrl from "./assets/images/facebook-cion.svg";
-import checkUrl from './assets/images/check-icon.svg';
-import { onMounted } from "vue";
+import { onMounted, watch, ref } from "vue";
 import {initTE, Input} from "tw-elements";
+import { useStore } from "vuex";
+import SchoolComponent from "./components/SchoolComponent.vue";
 
 onMounted(() => {
   initTE({Input});
-})
+});
+
+const store = useStore();
+const model = ref({
+});
+
+onMounted(() => {
+  model.value = {
+    ...store.state.schools
+  }
+});
+
+// const schools = computed(() => {
+//   return store.state.schools;
+// })
+
+
+// eslint-disable-next-line no-unused-vars
+watch(() => store.state.schools, (newVal, oldVal) => {
+  model.value = {
+    ...JSON.parse(JSON.stringify(newVal))
+  };
+  // console.log("newValn",newVal);
+  // console.log("App watch -> model.value", model.value);
+});
+
+
+
+
 </script>
 
 <style>
@@ -405,42 +285,5 @@ input {
 
 #school-container::-webkit-scrollbar {
   display: none;
-}
-#card::before {
-  position: absolute;
-  top: -20px;
-  left: -20px;
-  width: 40px;
-  height: 40px;
-  content:  " ";
-  background-image: url(./assets/images/logo.png);
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-color: rgba(255, 255, 255, 0.6);
-  border-radius: 22px;
-  z-index : 100;
-}
-
-#card {
-  box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.16);
-}
-
-.content {
-  clip-path: polygon(
-  0% 20px, 
-  3.4729px 19.6169px, 
-  6.84040px 18.793952px,
-  10px 17.32050px,
-  12.855752193730785px 15.32088886237956px,
-  15.32088886237956px 12.855752193730787px,
-  17.32050807568877px 10px,
-  18.793852415718167px  6.840402866513377px,
-  19.69615506024416px 3.472963553338608px,
-  20px 0%,
-  100% 0%,
-  100% 100%,
-  0% 100%
-  );
 }
 </style>
---
+++ src/components/SchoolComponent.vue
@@ -0,0 +1,155 @@
+
+<template>
+    <div
+    @click="picked_school"
+      class="
+      min-h-[176px]
+      relative
+      ml-5
+      flex-none
+      w-3/4
+      rounded-b-[10px]
+      rounded-tr-[10px]
+      "
+      :class="{'addborder' : school.choose}"
+      :style="[school.choose ? {'--addborder' : addborder} : {'--addborder' : 'none'}]"
+       id="card">
+        <div class="
+        content 
+        h-full 
+        relative 
+        pt-4 
+        pl-2
+        rounded-b-[10px]
+        rounded-tr-[10px]
+      bg-white/60
+      ">
+          <div class="text-block">
+              <h1 class="text-minor-sm text-school-name font-semibold">
+              {{ school.name }}
+              </h1>
+              <p class="
+              text-minor-xxsm 
+              line-clamp-6 
+              leading-[0.86875rem] 
+              text-school-name 
+              font-[0.6] 
+              mr-[0.5rem] 
+              mt-[0.51875rem]">
+              {{ school.description }}  
+            </p>
+          </div>
+
+          <a href="#" 
+          class="
+          absolute 
+          bottom-[0.625rem] 
+          flex 
+          items-center
+          justify-between
+          space-x-0.5
+          bg-[#6094E8]
+          left-2
+          rounded-[16px]">
+
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_142_5)">
+            <rect width="16" height="16" rx="8" fill="white"/>
+            <path fill="#1153BD" d="M16 8.02005C16 3.59298 12.416 0 8 0C3.584 0 0 3.59298 0 8.02005C0 11.9018 2.752 15.1338 6.4 15.8797V10.4261H4.8V8.02005H6.4V6.01504C6.4 4.46717 7.656 3.20802 9.2 3.20802H11.2V5.61403H9.6C9.16 5.61403 8.8 5.97494 8.8 6.41604V8.02005H11.2V10.4261H8.8V16C12.84 15.599 16 12.1825 16 8.02005Z"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_142_5">
+            <rect width="16" height="16" rx="8" fill="white"/>
+            </clipPath>
+            </defs>
+          </svg>
+
+            <span class="text-minor-xxsm text-white pr-1 font-semibold">
+              More
+            </span>
+          </a>
+          <!-- fill="#5D5D72" school-color -->
+          <span class="absolute bottom-[0.125rem] flex items-center right-[0.1975rem] w-4 h-4">
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0_120_7)">
+                <path :fill="school.choose ? '#1153BD' : '#5D5D72'" d="M8.60523 0.159912C3.9676 0.159912 0.203735 3.9454 0.203735 8.60965C0.203735 13.2739 3.9676 17.0594 8.60523 17.0594C13.2429 17.0594 17.0067 13.2739 17.0067 8.60965C17.0067 3.9454 13.2429 0.159912 8.60523 0.159912ZM6.32842 12.2346L3.31229 9.20113C3.2345 9.12291 3.1728 9.03003 3.13071 8.92782C3.08861 8.82561 3.06695 8.71606 3.06695 8.60543C3.06695 8.4948 3.08861 8.38525 3.13071 8.28303C3.1728 8.18082 3.2345 8.08795 3.31229 8.00972C3.39007 7.93149 3.48241 7.86944 3.58404 7.8271C3.68567 7.78476 3.79459 7.76297 3.90459 7.76297C4.01459 7.76297 4.12352 7.78476 4.22515 7.8271C4.32677 7.86944 4.41912 7.93149 4.4969 8.00972L6.92493 10.4432L12.7052 4.62983C12.8622 4.47183 13.0753 4.38308 13.2975 4.38308C13.5196 4.38308 13.7327 4.47183 13.8898 4.62983C14.0469 4.78782 14.1351 5.0021 14.1351 5.22553C14.1351 5.44897 14.0469 5.66325 13.8898 5.82124L7.51303 12.2346C7.43531 12.3129 7.34299 12.3751 7.24135 12.4175C7.13971 12.4599 7.03076 12.4817 6.92073 12.4817C6.8107 12.4817 6.70174 12.4599 6.60011 12.4175C6.49847 12.3751 6.40615 12.3129 6.32842 12.2346Z"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_120_7">
+                <rect x="0.203735" y="0.159912" width="16.803" height="16.8995" rx="8.40149" fill="white"/>
+                </clipPath>
+              </defs>
+            </svg>
+
+          </span>
+        </div>
+      </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+import { useStore } from 'vuex';
+
+// eslint-disable-next-line no-unused-vars
+const store = useStore();
+const props = defineProps({
+  school : Object,
+});
+
+const addborder = ref('1px solid #6094E8');
+
+
+function picked_school() {
+  store.dispatch('pick_school', props.school.id);
+}
+</script>
+
+<style>
+
+#card::after {
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  width: 40px;
+  height: 40px;
+  content:  " ";
+  background-image: url(/school-logo-fococlipping-standard.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: rgba(255, 255, 255, 0.6);
+  border-radius: 22px;
+  z-index : 100;
+  border : var(--addborder)
+}
+
+#card {
+  box-shadow: 3px -1px 4px rgba(0, 0, 0, 0.16);
+  
+}
+
+.content {
+  clip-path: polygon(
+  0% 20px, 
+  3.4729px 19.6169px, 
+  6.84040px 18.793952px,
+  10px 17.32050px,
+  12.855752193730785px 15.32088886237956px,
+  15.32088886237956px 12.855752193730787px,
+  17.32050807568877px 10px,
+  18.793852415718167px  6.840402866513377px,
+  19.69615506024416px 3.472963553338608px,
+  20px 0%,
+  100% 0%,
+  100% 100%,
+  0% 100%
+  );
+}
+
+.addborder {
+  border: 1px solid #6094E8;
+}
+
+
+</style>
